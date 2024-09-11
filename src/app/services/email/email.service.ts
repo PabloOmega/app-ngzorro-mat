d7f5561631ae1f7c9ac9b6a306055a96c47abca0
PabloOmega <pablonavarretectdb1c@gmail.com>
2024-09-11 08:25:39 -0500

    modified:   src/app/services/email/email.service.ts

--- src/app/services/email/email.service.ts
+++ src/app/services/email/email.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class EmailService {
   private apiUrl = 'https://api.brevo.com/v3/smtp/email';
-  private apiKey = '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';
+  private apiKey = '';
 
   constructor(private http: HttpClient) {}
 
